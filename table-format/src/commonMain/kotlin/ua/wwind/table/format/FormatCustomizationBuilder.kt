ee16b534639f8567cffe5d3a025a1a814236c692
xelon <og@wwind.ua>
2026-09-25 20:12:47 +0300

    Revert "test: temporary CI canary (to be reverted)"
    This reverts commit 51dcf64d752522c6e2196eb83fdcec60332aaa4a.

--- table-format/src/commonMain/kotlin/ua/wwind/table/format/FormatCustomizationBuilder.kt
+++ table-format/src/commonMain/kotlin/ua/wwind/table/format/FormatCustomizationBuilder.kt
@@ -1,6 +1,5 @@
 package ua.wwind.table.format
 
-
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
--- table-format/src/commonTest/kotlin/ua/wwind/table/format/CiCanaryTest.kt
+++
@@ -1,9 +0,0 @@
-package ua.wwind.table.format
-
-import kotlin.test.Test
-import kotlin.test.fail
-
-class CiCanaryTest {
-    @Test
-    fun canary(): Unit = fail("CI canary: this test must fail")
-}
